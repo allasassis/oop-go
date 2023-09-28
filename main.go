8a3d61b8f46a51c2e55b0eb8ebbcf125f9ddc794
Allas Assis <allasassis10@gmail.com>
2023-09-28 20:48:16 -0300

    Pointers implemented with structs

--- main.go
+++ main.go
@@ -13,4 +13,8 @@ func main() {
 	account := CurrentAccount{"Allas", 581, 10941, 0}
 	account1 := CurrentAccount{name: "John", balance: 41.1}
 	fmt.Println(account, account1)
+
+	var account2 *CurrentAccount = new(CurrentAccount)
+	account2.name = "Luke"
+	fmt.Println(*account2)
 }
