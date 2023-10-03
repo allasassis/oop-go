280773f3539f7cf522803ec6e94c0e9b358e6886
Allas Assis <allasassis10@gmail.com>
2023-10-02 21:37:18 -0300

    Transfer method created

--- main.go
+++ main.go
@@ -16,8 +16,10 @@ func main() {
 	account.balance = 500
 	account.agencyNumber = 8231
 
-	message := account.withdraw(200)
-	fmt.Println(message)
+	account2 := CurrentAccount{"Luke", 3212, 321, 1000}
+
+	fmt.Println(account2.transfer(832, &account))
+	fmt.Println("Current balance:", account.balance)
 }
 
 func (account *CurrentAccount) withdraw(value float64) string {
@@ -26,5 +28,23 @@ func (account *CurrentAccount) withdraw(value float64) string {
 	}
 
 	account.balance -= value
-	return "Your withdrawal has been successfully processed! And now your balance is: " + fmt.Sprintf("%.2f", account.balance)
+	return "Your withdrawal has been processed successfully! Your new balance is now: " + fmt.Sprintf("%.2f", account.balance)
+}
+
+func (account *CurrentAccount) deposit(value float64) (string, float64) {
+	if value > 0 {
+		account.balance += value
+		return "Your deposit has been processed successfully! Your new balance is now: ", account.balance
+	}
+	return "Unfortunately, you cannot make a deposit because the value is less than zero. Your balance remains unchanged: ", account.balance
+}
+
+func (account *CurrentAccount) transfer(transferValue float64, destinyAccount *CurrentAccount) string {
+	if transferValue > 0 && account.balance < transferValue {
+		return "Unfortunately, you cannot make a withdrawal because your balance is lower than your request."
+	} else {
+		account.withdraw(transferValue)
+		destinyAccount.deposit(transferValue)
+		return "Your transfer has been processed successfully! Your new balance is now: " + fmt.Sprintf("%.2f", account.balance)
+	}
 }
